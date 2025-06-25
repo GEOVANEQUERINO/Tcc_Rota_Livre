9c93d177c0ccb33f4fd2144a35f30725b2b468e8
Victoralexandre113 <vectorscosta80@gmail.com>
2025-06-24 21:41:08 -0300

    tcc

--- Victor/bike.js
+++ Victor/bike.js
@@ -3,12 +3,12 @@
 const swiper = new Swiper('.swiper', {//Inicializa o carerrosel na classe ".swiper" usando a biblioteca swiper
     loop: true, //faz com que as imagens fique se repetindo repetidamente
     autoplay: { //configuração da reprodução automatica do Carrosel
-    delay: 2000, // tempo para mudar de slide (2 segundos)
+    delay: 1000, // tempo para mudar de slide (2 segundos)
     disableOnInteraction: false, //continua o autoplay mesmo se o usuário interagir (ex: tentar arrastar)
 },  
     effect: 'fade',
     fadeEffect: {croosfade: true}, //faz o slide anterior desaparecer de maneira suave enquanto o outro aparece
 
-    allowTouchMove: true, //Impede que o usuário use o toque (arraste) para o slide 
+    allowTouchMove: false, //Impede que o usuário use o toque (arraste) para o slide 
 
-})
+});
