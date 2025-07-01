0bafe858d81937e6224cd7560b4f74f385ed754b
gabrielbastos02 <kamasuabiel@gmail.com>
2025-07-01 19:55:39 -0300

    diversao
    gbf

--- GBF/diversao.js
+++ GBF/diversao.js
@@ -88,9 +88,23 @@ document.addEventListener('DOMContentLoaded', () => {
     observer.observe(section);
 });
 
-const toggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
 
-toggle.addEventListener('click', () => {
-  menu.classList.toggle('active');
+// MENU HAMBURGUER
+
+
+//const toggle = document.getElementById('menu-toggle');
+//const menu = document.getElementById('menu');
+
+//toggle.addEventListener('click', () => {
+  //menu.classList.toggle('active');
+//});
+// hamburguerButton
+// Aqui adicionamos o evento do click sobre o ícone de hamburguer
+hamburgerButton.addEventListener("click", function() {
+    mobileMenu.classList.add("flex"); // função que adiciona uma classe (flex). que esta fazendo a nossa caixa DIV visivel, ou seja ficara visivel para todos
+});
+
+// criado a função que remove a classe, ou seja ficara invisivel
+closeButton.addEventListener("click", function(){
+    mobileMenu.classList.remove("flex");
 });
