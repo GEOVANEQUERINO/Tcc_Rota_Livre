48eee22013b4dd4530b2733f49d9934845b588ff
Victoralexandre113 <vectorscosta80@gmail.com>
2025-06-30 21:22:37 -0300

    TCC
    TCC

--- GBF/diversao.js
+++ GBF/diversao.js
@@ -87,3 +87,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     observer.observe(section);
 });
+
+const toggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
