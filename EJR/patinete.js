72ebb26d1d633b187432cf95480896b841250244
Euclides Leonardo <leonardojr2k07@gmail.com>
2025-07-01 16:39:55 -0300

    patinete

---
+++ EJR/patinete.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {//Inicializa o carerrosel na classe ".swiper" usando a biblioteca swiper
+    loop: true, //faz com que as imagens fique se repetindo repetidamente
+    autoplay: { //configuração da reprodução automatica do Carrosel
+    delay: 2000, // tempo para mudar de slide (2 segundos)
+    disableOnInteraction: false, //continua o autoplay mesmo se o usuário interagir (ex: tentar arrastar)
+},  
+    effect: 'fade',
+    fadeEffect: {croosFade: true}, //faz o slide anterior desaparecer de maneira suave enquanto o outro aparece
+
+    allowTouchMove: false, //Impede que o usuário use o toque (arraste) para o slide 
+
+});
