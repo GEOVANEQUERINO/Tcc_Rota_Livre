93b95640a043939a8614aae57f67f4638af02dad
EuclidesLeo2007 <leonardojr2k07@gmail.com>
2025-07-01 21:01:22 -0300

    hufugyjg

--- EJR/patinete.js
+++ EJR/patinete.js
@@ -10,9 +10,22 @@ const swiper = new Swiper('.swiper', {//Inicializa o carerrosel na classe ".swip
     allowTouchMove: false, //Impede que o usuário use o toque (arraste) para o slide 
 
 });
-const toggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
+// MENU HAMBURGUER
 
-toggle.addEventListener('click', () => {
-  menu.classList.toggle('active');
+
+//const toggle = document.getElementById('menu-toggle');
+//const menu = document.getElementById('menu');
+
+//toggle.addEventListener('click', () => {
+  //menu.classList.toggle('active');
+//});
+// hamburguerButton
+// Aqui adicionamos o evento do click sobre o ícone de hamburguer
+hamburgerButton.addEventListener("click", function() {
+  mobileMenu.classList.add("flex"); // função que adiciona uma classe (flex). que esta fazendo a nossa caixa DIV visivel, ou seja ficara visivel para todos
+});
+
+// criado a função que remove a classe, ou seja ficara invisivel
+closeButton.addEventListener("click", function(){
+  mobileMenu.classList.remove("flex");
 });
